72a523fa1e268669dd3a1629dcc0c3b4548a27d5
DESKTOP-3DSJ48T\ANTEC <dan.monceau.lpcb@gmail.com>
2024-01-28 20:51:23 +0100

    first commit

---
+++ src/main/java/fr/danmonceau/fr/DanMain.java
@@ -0,0 +1,10 @@
+package fr.danmonceau.fr;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DanMain extends JavaPlugin {
+
+    public void onEnable() {
+        System.out.println("Plugin Activate");
+    }
+}
